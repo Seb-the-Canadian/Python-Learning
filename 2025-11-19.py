feab8d253c15e7beba373746be3d09a3c064e154
Seb the Canadian. <seb@cognitivearchitecture.ca>
2025-11-19 22:40:24 -0500

    Started working with loops.

---
+++ 2025-11-19.py
@@ -0,0 +1,24 @@
+# 2025-11-19.py
+
+# Ingredients list
+# ingredients = ["pasta", "tomatoes", "garlic", "basil", "olive oil", "salt"]
+
+# Loop thorugh and print each ingredient
+# for ingredient in ingredients:
+# print(ingredient)
+
+# quantities = [1000, 800, 40, 30, 30, 15]
+
+# for qty in quantities:
+# Check if quantity is more than 500
+# if qty > 500:
+# print("Plenty in stock")
+# elif qty >= 100:
+# print("Enough for a small portion")
+# else:
+# print("Nearly out!")
+
+# ingredient_name = "pasta"
+# Loop through each character
+# for letter in ingredient_name:
+# print(letter)
