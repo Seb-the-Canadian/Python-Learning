8e49ac7c4d874f0a85f15b71e33dce5fcc002c72
Seb the Canadian. <seb@cognitivearchitecture.ca>
2025-11-22 21:05:34 -0500

    Daily practice file - starting while loops

---
+++ 2025-11-22.py
@@ -0,0 +1,10 @@
+# 2025-11-22.py
+# While loops
+
+ingredients_to_add = 5
+items_added = 0
+# Keep adding while we have items left.
+# while items_added < ingredients_to_add:
+# items_added += 1
+# remaining = ingredients_to_add - items_added
+# print(remaining, "ingredients left to add")
